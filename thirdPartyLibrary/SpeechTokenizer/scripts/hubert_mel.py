460f24e77f00ff3ffbec75fc7cedeb360b57afb8
rongkunxue <rongkunxue@outlook.com>
2025-01-21 05:27:04 +0000

    polish

--- thirdPartyLibrary/SpeechTokenizer/scripts/hubert_mel.py
+++ thirdPartyLibrary/SpeechTokenizer/scripts/hubert_mel.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', '-c', type=str, default="config/spt_base_cfg.json")
-    parser.add_argument('--rep_dir', type=str, default="/mnt/nfs3/zhangjinouwen/dataset/rep/hubert")
+    parser.add_argument('--rep_dir', type=str, default="/mnt/nfs3/zhangjinouwen/dataset/rep/rep_small_avg_hubert")
     parser.add_argument('--exts', type=str, help="Audio file extensions, splitting with ','", default='wav,mp3')
     parser.add_argument('--split_seed', type=int, help="Random seed", default=0)
     parser.add_argument('--valid_set_size', type=float, default=1500)
@@ -35,16 +35,12 @@ if __name__ == '__main__':
         for path in [path2] 
         for file in path.glob(f'**/*.{ext}')
     ]
-    if args.valid_set_size != 0 and args.valid_set_size < 1:
-        valid_set_size = int(len(file_list) * args.valid_set_size)
-    else:
-        valid_set_size = int(args.valid_set_size)
     train_file_list = "/mnt/nfs3/zhangjinouwen/dataset/rep/rep_small_mel_hubert_train.txt"
-    valid_file_list = "/mnt/nfs3/zhangjinouwen/dataset/rep/rep_small_mel_hubert_eval.txt"
+
     segment_size = 65536
     random.seed(args.split_seed)
     random.shuffle(file_list)
-    print(f'A total of {len(file_list)} samples will be processed, and {valid_set_size} of them will be included in the validation set.')
+    print(f'A total of {len(file_list)} samples will be processed.')
     with torch.no_grad():
         for i, audio_file in tqdm(enumerate(file_list)):
             wav_24k, sr = torchaudio.load(audio_file)
@@ -56,24 +52,18 @@ if __name__ == '__main__':
                 wav_16k = torchaudio.functional.resample(wav_24k, sr, 16000)
             input_values = feature_extractor(wav_16k.squeeze(0), sampling_rate=16000, return_tensors="pt").input_values
             ouput = model(input_values.to(model.device), output_hidden_states=True)
-            if target_layer == 'avg':
-                rep = torch.mean(torch.stack(ouput.hidden_states), axis=0)
-            else:
-                rep = ouput.hidden_states[target_layer]
+            rep = torch.mean(torch.stack(ouput.hidden_states), axis=0)
+           
 
             if str(path2) in audio_file:
-                rep_file = audio_file.replace(str(path2), f"{args.rep_dir}/clean100").split('.')[0] +'.hubert.npy'
+                rep_file = audio_file.replace(str(path2), f"{args.rep_dir}/cleanmel100").split('.')[0] +'.hubert.npy'
             
             rep_sub_dir = '/'.join(rep_file.split('/')[:-1])
             if not os.path.exists(rep_sub_dir):
                 os.makedirs(rep_sub_dir)
             np.save(rep_file, rep.detach().cpu().numpy())
-            if i < valid_set_size:
-                with open(valid_file_list, 'a+') as f:
-                    f.write(f'{audio_file}\t{rep_file}\n')
-            else:
-                with open(train_file_list, 'a+') as f:
-                    f.write(f'{audio_file}\t{rep_file}\n')
+            with open(train_file_list, 'a+') as f:
+                f.write(f'{audio_file}\t{rep_file}\n')
             
             
 
